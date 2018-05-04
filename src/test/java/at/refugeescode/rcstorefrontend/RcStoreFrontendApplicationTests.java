2223fb4eb30d589e1ea274f7032b09e153487331
Fozat Kardouh <fozatkardouh@gmail.com>
2018-05-04 11:48:27 +0200

    added empty project

---
+++ src/test/java/at/refugeescode/rcstorefrontend/RcStoreFrontendApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.rcstorefrontend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RcStoreFrontendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
